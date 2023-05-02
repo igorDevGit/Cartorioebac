7b8e29160fb00fa534351b4db97084a470fa8393
igorDevGit <132406545+igorDevGit@users.noreply.github.com>
2023-05-02 18:57:49 -0300

    Upload do projeto
    Projeto completo de acordo com as especificações do cliente

---
+++ lurdinha .c
@@ -0,0 +1,164 @@
+#include <stdio.h> //biblioteca de comunicação com o usuario
+#include <stdlib.h> // biblioteca de alocação de espaço em memória
+#include <locale.h> //biblioteca de alocações de texto por região
+#include <string.h> //biblioteca resposável por cuidar das string
+
+int registro()//função responsavel por cadastrar os usuários no sistema
+{
+	//inicio criação de variaveis/string
+	char arquivo[40];
+	char cpf[40];
+	char nome[40];
+	char sobrenome[40];
+    char cargo[40];
+	//final da criação de variaveis/string
+	
+	printf("Digite o CPF a ser cadastrado;");//Coletando informações do usuário
+	scanf("%s", cpf); //"%s" refere-se a string
+	
+	strcpy(arquivo, cpf); //Responsavel por copiar os valores das string
+	
+	FILE *file; // cria o arquivo
+	file = fopen(arquivo, "w"); //cria o arquivo e o "w" significa escrever
+	fprintf(file,cpf); // salva o valor da variavel 
+	fclose(file); // fecha o arquivo
+	
+	file = fopen(arquivo,"a"); //Cria o arquivo e o "a" significa atualizar
+	fprintf(file,","); //salva o valor da variavel
+	fclose(file); //Fecha o arquivo
+	
+	printf("Digite o nome a ser cadastrado: "); //Coletando informações do usuário
+	scanf("%s",nome); //"%s" refere-se a string
+	
+	file = fopen(arquivo,"a"); //Cria o arquivo e o "a" significa atualizar
+	fprintf(file,nome); //Salva o valor da variavel
+	fclose(file); //Fecha o arquivo
+	
+	file = fopen(arquivo, "a"); //Cria o arquivo e o "a" significa atualizar
+	fprintf(file, ","); //Salva o valor da variavel
+	fclose(file); //Fecha o arquivo
+	
+	printf("Digite o sobrenome a ser cadastrado: "); //Coletando informações do usuário
+	scanf("%s", sobrenome); //"%s" refere-se a string 
+	
+	file = fopen(arquivo, "a"); //Cria o arquivo e o "a" significa atualizar
+	fprintf(file,sobrenome); //salva o valor da variavel
+	fclose(file); //Fecha o arquivo
+	
+	file = fopen(arquivo, "a"); //Cria o arquivo e o "a" significa atualizar
+	fprintf(file, ","); //Salva o valor da variavel
+	fclose(file); //Fecha o arquivo
+	
+	printf("Digite o cargo a ser cadastrado; "); //Coletando informações do usuário
+	scanf("%s", cargo); //"%s" refere-se a string
+	
+	file = fopen(arquivo, "a"); //Cria o arquivo e o "a" significa atualizar
+	fprintf(file,cargo); //Salva o valora da variavel
+	fclose(file); //Fecha o arquivo
+	
+	system("pause"); //Pausa a tela
+	
+	
+	
+	
+}
+
+int consulta()
+{
+	setlocale(LC_ALL, "portuguese"); //definindo a linguagem
+	
+	//inicio criação de variavel /string
+	char cpf[40];
+	char conteudo[200];
+	//Fim da criação de variavel /string
+	printf("Digite o CPF a ser consultado: "); //Coletando informações do usuário
+	scanf("%s",cpf); //"%s" refere-se a string
+	
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("Não foi possivel abrir o arquivo, nãp localizado!.\n");
+	}
+	
+    while(fgets(conteudo, 200, file) != NULL)
+	{
+		printf("\nEssas são as informações do usuário: \n");
+		printf("%s", conteudo);
+		printf("\n\n");
+	}
+	
+	system("pause");
+}
+
+int deletar()
+{
+	char cpf[40];
+	
+	printf("Digite o CPF a ser deletado: ");
+	scanf("%s",cpf);
+	
+	remove(cpf);
+	
+	FILE *file;
+	file = fopen(cpf,"r");
+	
+	if(file == NULL)
+	{
+		printf("O usuário não se encontra no sistema!.\n");	
+	}
+	system("pause");
+	
+}
+
+int main()
+{
+	int opcao=0; //definindo as variáveis
+	int laco=1;
+	
+	for(laco=1;laco=1;)
+	{
+		
+		system("cls");
+	
+    	setlocale(LC_ALL, "portuguese"); //definindo a linguagem
+	
+	    printf("### Cartorio da EBAC ###\n\n"); // Inicio do menu
+	    printf("escolha a opcao desejada no menu:\n\n");
+	    printf("\t1 - Registrar nomes\n");
+	    printf("\t2 - Consultar nomes\n");
+    	printf("\t3 - Deletar nomes\n\n"); 
+	    printf("opção: "); //Fim do menu
+	    
+    	scanf("%d", &opcao); //Armazenando a escolha do usuário
+	
+    	system("cls"); //Responsavel por limpar a tela
+    	
+    	
+    	switch(opcao) //Inicio da seleção do menu
+    	{
+    		case 1:
+    		registro(); //Chamada de funções
+		    break;
+		    
+		    case 2:
+		    consulta();
+		    break;
+		    
+		    case 3:
+		   deletar();  	
+		    break;
+		    
+		    default:
+		    printf("Essa opção não está disponivel!\n");
+	        system("pause");
+	        break;
+		}
+	
+	   
+
+    }
+ 
+}
